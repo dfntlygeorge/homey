ae5b16023aefcdcd18c12beafba23fb8cf75a23b
dfntlygeorge <g.a.donayre.business@gmail.com>
2025-07-01 20:31:07 +0800

    fix: why was i using 'NEXT_PUBLIC' to server-only enviroment variables?

--- env.mjs
+++ env.mjs
@@ -9,17 +9,18 @@ export const env = createEnv({
     AUTH_GOOGLE_SECRET: z.string(),
     UPSTASH_REDIS_REST_URL: z.string().url(),
     UPSTASH_REDIS_REST_TOKEN: z.string(),
-    HOSTNAME: z.string().optional(), // optional since sometimes not always present
-    PORT: z.coerce.number().optional(), // convert from string to number if present
+    HOSTNAME: z.string().optional(),
+    PORT: z.coerce.number().optional(),
+
+    AWS_S3_REGION: z.string(),
+    AWS_S3_ACCESS_KEY_ID: z.string(),
+    AWS_S3_SECRET_ACCESS_KEY: z.string(),
+    AWS_S3_BUCKET_NAME: z.string(),
   },
 
   client: {
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: z.string(),
     NEXT_PUBLIC_APP_URL: z.string().url(),
-    NEXT_PUBLIC_AWS_S3_REGION: z.string(),
-    NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID: z.string(),
-    NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY: z.string(),
-    NEXT_PUBLIC_AWS_S3_BUCKET_NAME: z.string(),
     NEXT_PUBLIC_MAPTILER_API_KEY: z.string(),
   },
 
@@ -29,19 +30,18 @@ export const env = createEnv({
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REST_TOKEN,
     HOSTNAME: process.env.HOSTNAME,
     PORT: process.env.PORT,
+
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_S3_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID,
+    AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
 
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    NEXT_PUBLIC_AWS_S3_REGION: process.env.NEXT_PUBLIC_AWS_S3_REGION,
-    NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID:
-      process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID,
-    NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY:
-      process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY,
-    NEXT_PUBLIC_AWS_S3_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
     NEXT_PUBLIC_MAPTILER_API_KEY: process.env.NEXT_PUBLIC_MAPTILER_API_KEY,
   },
 });
