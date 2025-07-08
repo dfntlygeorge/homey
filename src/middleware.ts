1af905b59df36342a8b825a8d0aa43e14767e98b
dfntlygeorge <g.a.donayre.business@gmail.com>
2025-07-08 21:47:29 +0800

    removed onboarding page, made user role required with default as student.

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,20 +7,8 @@ export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const userRole = session?.user?.role;
 
-  const isReservePage =
-    pathname.startsWith("/listings/") && pathname.includes("/reserve");
-  const isOnboardingPage = pathname === "/onboarding";
   const isAdmin = userRole === "ADMIN";
 
-  // 👮 Redirect unauthenticated users trying to reserve
-  if (!session && isReservePage) {
-    return NextResponse.redirect(new URL("/auth/sign-in", req.url));
-  }
-
-  // 👋 Redirect signed-in users without role to onboarding
-  if (session && !userRole && !isOnboardingPage) {
-    return NextResponse.redirect(new URL("/onboarding", req.url));
-  }
   if (!isAdmin && pathname.startsWith(routes.admin)) {
     console.log("Redirecting non-admin to listings");
     return NextResponse.redirect(new URL("/listings", req.url));
@@ -30,5 +18,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/listings/:path*", "/admin"],
+  matcher: ["/admin"],
 };
